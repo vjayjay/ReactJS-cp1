1a236d605bc84b9eb490626478562b4cfe132428
viky <nxtwave@ip-10-2-35-30.ap-south-1.compute.internal>
2024-08-05 11:35:58 +0000

    solution

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  <div className='bg-img'>
+    <h1 className='text'>Congratulations</h1>
+    <div className='bg-1'>
+      <img
+        className=''
+        src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png'
+      />
+      <h1 className='hp'>Kiran V</h1>
+      <p className='hp'>Rama instituta of technology at vikunta</p>
+      <img
+        className=' '
+        src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png'
+      />
+    </div>
+  </div>
+)
+ReactDOM.render(element, document.getElementById('root'))
